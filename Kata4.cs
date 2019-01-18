6bce9eb688b6f55d1f2e17536b975600dd6ff3d1
Anton Patron <patronant@gmail.com>
2019-01-18 18:07:48 +0100

    First

--- Kata4.cs
+++ Kata4.cs
@@ -19,35 +19,77 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace kata {
     public static class Kata4 {
 
-        public static int DblLinear (int n) {
+        public static async Task<int> DblLinear (int n) {
             if (n == 0)
                 return 1;
 
-            var l = Iterate (new List<int> { 1 }, 0, n);
-            return l.ElementAt (n);
+            Task<List<int>> yT = new List<int> { 1 }.Iter (CalculateY, n);
+            Task<List<int>> zT = new List<int> { 1 }.Iter (CalculateZ, n);
+            Task.WhenAll (new List<Task<List<int>>> { zT, yT });
+            var yL = await yT;
+            var zl = await zT;
+
+            Task<List<int>> yXT2 = yL.XrossLists (CalculateZ);
+            Task<List<int>> zXT2 = zl.XrossLists (CalculateY);
+            Task.WhenAll (new List<Task<List<int>>> { yXT2, zXT2 });
+            var yL2 = await yXT2;
+            var zl2 = await zXT2;
+
+            var xl = yL.Concat (zl).Concat (yL2).Concat (zl2).Distinct ().ToList ();
+            xl.Sort ();
+            return xl.ElementAt (n);
         }
 
-        private static List<int> Iterate (List<int> list, int cursor, int n) {
-            if (n == 0)
+        private static async Task<List<int>> XrossLists (this List<int> list, Func<int, int> func) {
+            return list.Select (x => func (x)).ToList ();
+        }
+        public static IEnumerable<int> Calculate (List<int> list, Func<int, int> func) {
+            return list.Select (x => func (x)).ToList ();
+        }
+
+        public static async Task<List<int>> Iter (this List<int> list, Func<int, int> func, int iterations) {
+            if (iterations == 0)
                 return list;
+            iterations--;
 
-            list.Add (CalculateY (list[cursor]));
-            list.Sort ();
-            list.Add (CalculateZ (list[cursor]));
-            list.Sort ();
-
-            cursor++;
-            n--;
-            return Iterate (list, cursor, n);
+            list.Add (func (list.LastOrDefault ()));
+            return await list.Iter (func, iterations);
         }
 
         public static Func<int, int> CalculateY => (x) => 2 * x + 1;
         public static Func<int, int> CalculateZ => (x) => 3 * x + 1;
+        // private static List<int> Iterate (List<int> list, int cursor, int n) {
+        //     if (n == 0)
+        //         return list;
+
+        //     list.Insert (CalculateY (list[cursor]));
+        //     n--;
+
+        //     list.Add (CalculateZ (list[cursor]));
+        //     n--;
+
+        //     cursor++;
+        //     return Iterate (list, cursor, n);
+        // }
+
+        public static List<int> Iterate2 (List<int> list, int n) {
+            if (n == 0)
+                return list;
+
+            n--;
+
+            List<int> newList = new List<int> ();
+            newList.AddRange (Calculate (list, CalculateY));
+            newList.AddRange (Calculate (list, CalculateZ));
+            return Iterate2 (newList, n);
+            // return Iterate2 (list, n);
+        }
 
     }
 
@@ -55,7 +97,7 @@ namespace kata {
     public class Tests4 {
 
         [TestCase (22, 10)]
-        [TestCase (175, 50)]
+        [TestCase (91, 30)]
         public void DblLinear (int result, int x) => Assert.AreEqual (result, Kata4.DblLinear (x));
     }
 
