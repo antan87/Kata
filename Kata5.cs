2375af596d5fb4bec79e983241b4b2b48b869489
Anton Patron <anton.patron@aareon.com>
2019-01-29 22:09:08 +0100

    Commit Kata 5 update.

--- Kata5.cs
+++ Kata5.cs
@@ -30,25 +30,37 @@ namespace kata {
                     condtion = false;
 
             }
+            if (x != 0) {
+                var firstArr = array.ToList ().Where ((item, ind) => ind < x).ToArray ();
+                var secondArr = array.ToList ().Where ((item, ind) => ind >= x).ToArray ();
+                // array = array.Except (firstArr).ToArray ();
+                // firstArr.ToList ().ForEach (v => Console.WriteLine (v));
+                // Console.WriteLine ("First");
+                // array.ToList ().ForEach (v => Console.WriteLine (v));
+                Array.Sort (secondArr);
+                // array.ToList ().ForEach (v => Console.WriteLine (v));
+                var nA = firstArr.Concat (secondArr);
 
-            condtion = x + 1 != array.Length;
-            while (condtion) {
-
-                if (array[x + 1] < array[x]) {
-                    Int64 temp = array[x];
-                    array.ToList ().ForEach (xc => Console.WriteLine ("X" + x + " Nr " + xc));
-                    array[x] = array[x + 1];
-                    array[x + 1] = temp;
-                }
-                x++;
-
-                if (x + 1 == array.Length)
-                    condtion = false;
+                string newNumber = string.Empty;
+                nA.ToList ().ForEach (xs => { newNumber += xs; });
+                var ss = Convert.ToInt64 (newNumber);
+                return ss == n ? -1 : ss;
             }
-            string newNumber = string.Empty;
-            array.ToList ().ForEach (xs => { newNumber += xs; });
-            var ss = Convert.ToInt64 (newNumber);
-            return ss == n ? -1 : ss;
+            // condtion = x + 1 != array.Length;
+            // while (condtion) {
+
+            //     if (array[x + 1] < array[x]) {
+            //         Int64 temp = array[x];
+            //         array.ToList ().ForEach (xc => Console.WriteLine ("X" + x + " Nr " + xc));
+            //         array[x] = array[x + 1];
+            //         array[x + 1] = temp;
+            //     }
+            //     x++;
+
+            //     if (x + 1 == array.Length)
+            //         condtion = false;
+            // }
+            return -1;
         }
     }
 
@@ -58,6 +70,9 @@ namespace kata {
         [TestCase (2071, 2017)]
         [TestCase (-1, 9999999999)]
         [TestCase (7012, 2710)]
+        [TestCase (414, 144)]
+        [TestCase (441, 414)]
+        [TestCase (59884848483559, 5988484848359)]
         public void NextBiggerNumber (long result, long n) => Assert.AreEqual (result, Kata5.NextBiggerNumber (n));
     }
 }
