4f38aad2d43c9939bb29304a28cc870ce03e0786
Anton Patron <anton.patron@aareon.com>
2019-02-07 20:02:11 +0100

    Start building battleship yard.

--- Kata7.cs
+++ Kata7.cs
@@ -1,5 +1,3 @@
-using System.Collections.Immutable;
-using System.Linq;
 // Write a method that takes a field for well-known board game "Battleship" as an argument and returns true if it has a valid disposition of ships, false otherwise. Argument is guaranteed to be 10*10 two-dimension array. Elements in the array are numbers, 0 if the cell is free and 1 if occupied by ship.
 
 // Battleship (also Battleships or Sea Battle) is a guessing game for two players. Each player has a 10x10 grid containing several "ships" and objective is to destroy enemy's forces by targetting individual cells on his field. The ship occupies one or more cells in the grid. Size and number of ships may differ from version to version. In this kata we will use Soviet/Russian version of the game.
@@ -15,9 +13,7 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
-
 namespace kata {
-
     public class BattleshipField {
         public static bool ValidateBattlefield (int[, ] field) {
 
